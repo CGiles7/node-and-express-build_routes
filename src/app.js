f32bbbcfd3ec39592612a226a9468b62794a2787
Callie Giles <anchoredinthecrown@gmail.com>
2023-08-09 01:02:14 -0400

    add routes in app

--- src/app.js
+++ src/app.js
@@ -3,5 +3,15 @@ const express = require("express");
 const app = express();
 
 // Build your routes here
+const sayPong = (req, res, next) => {
+  res.send("pong!");
+};
+
+const welcomeMessage = (req, res, next) => {
+  res.send("Welcome to my server.");
+};
+
+app.get("/ping", sayPong);
+app.get("/welcome", welcomeMessage);
 
 module.exports = app;
